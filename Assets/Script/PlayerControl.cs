167646b02dba4395bb21e48eb4be88329a44e691
Smogzz <144461630+Smogzz@users.noreply.github.com>
2024-09-16 12:35:59 -0400

    Camera Position

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
