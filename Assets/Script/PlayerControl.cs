197c5ad78a0c631ee461977ad4f386d729af96cd
Smogzz <144461630+Smogzz@users.noreply.github.com>
2024-09-18 11:59:41 -0400

    Pedal to the metal

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float speed = 20.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float turnSpeed = 20.0f;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Move Vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed); 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); 
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
